3c2b25277e3ef4fe22e4e011135da2aa4ac90c8c
Bushraabir <186773951+Bushraabir@users.noreply.github.com>
2025-07-25 12:08:51 +0600

    commit

--- 16. 3Sum Closest.cpp
+++ 16. 3Sum Closest.cpp
@@ -1,32 +1,36 @@
 class Solution {
 public:
     int threeSumClosest(vector<int>& nums, int target) {
-        sort(nums.begin(), nums.end());
         int n = nums.size();
-        int closestSum = nums[0] + nums[1] + nums[2]; 
+       
+        sort(nums.begin(), nums.end());
+        
+        
+        int resultSum = nums[0] + nums[1] + nums[2];
+        int minDiff = INT_MAX;  
 
-        for(int i = 0; i < n - 2; i++) {
+        for (int i = 0; i < n - 2; i++) {
+           
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+
             int j = i + 1;
             int k = n - 1;
+            while (j < k) {
+                int sum = nums[i] + nums[j] + nums[k];
 
-            while(j < k) {
-                int currSum = nums[i] + nums[j] + nums[k];
+               
+                if (sum == target) return target;
+                else if (sum < target) j++;   
+                else k--;                   
 
+                int diffToTarget = abs(sum - target);
                 
-                if(abs(currSum - target) < abs(closestSum - target)) {
-                    closestSum = currSum;
-                }
-
-                
-                if(currSum < target) {
-                    j++; 
-                }
-                else {
-                    k--; 
+                if (diffToTarget < minDiff) {
+                    resultSum = sum;
+                    minDiff = diffToTarget;
                 }
             }
         }
-
-        return closestSum;
+        return resultSum;
     }
 };
