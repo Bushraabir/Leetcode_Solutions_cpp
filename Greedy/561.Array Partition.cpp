31a7bc5a8c83f363487588a2f4779de375f55184
Bushraabir <186773951+Bushraabir@users.noreply.github.com>
2026-03-26 09:59:48 +0600

    updated

---
+++ Greedy/561.Array Partition.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int arrayPairSum(vector<int>& nums) {
+
+        vector<int> freq(20001, 0);
+
+        // Count frequencies
+        for(int num : nums){
+            freq[num + 10000]++;
+        }
+
+        int sum = 0;
+        bool take = true;
+
+        // Traverse numbers in sorted order
+        for(int i = 0; i < 20001; i++){
+
+            while(freq[i] > 0){
+
+                if(take)
+                    sum += (i - 10000);
+
+                take = !take;
+                freq[i]--;
+            }
+        }
+
+        return sum;
+    }
+};
